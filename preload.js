56ca5e54e01fcaeedae1f9c3b5d671d948577023
zhinouno-ui <zhinouno@gmail.com>
2026-03-01 09:03:57 -0300

    Fix window lifecycle for tray reopen and disable forced autostart

--- preload.js
+++ preload.js
@@ -12,16 +12,25 @@ contextBridge.exposeInMainWorld('nexoStore', {
 
 contextBridge.exposeInMainWorld('electronAPI', {
   openExternal: (url) => ipcRenderer.invoke('external:open', url),
+  openImportDialog: () => ipcRenderer.invoke('dialog:openImportFiles'),
+  notify: (payload) => ipcRenderer.invoke('app:notify', payload || {}),
   checkForUpdates: () => ipcRenderer.invoke('updater:check'),
   installUpdate: (options) => ipcRenderer.invoke('updater:install', options || {}),
   rollbackPreviousVersion: () => ipcRenderer.invoke('updater:rollbackPrevious'),
+  getUpdaterDiagnostics: () => ipcRenderer.invoke('updater:diagnostics'),
   getAppVersion: () => ipcRenderer.invoke('app:getVersion'),
+  getRuntimeHash: () => ipcRenderer.invoke('app:getRuntimeHash'),
   getErrorLogPath: () => ipcRenderer.invoke('app:getErrorLogPath'),
   openErrorLog: () => ipcRenderer.invoke('app:openErrorLog'),
   logError: (payload) => ipcRenderer.invoke('app:logError', payload),
   zoomIn: () => ipcRenderer.invoke('zoom:in'),
   zoomOut: () => ipcRenderer.invoke('zoom:out'),
   zoomReset: () => ipcRenderer.invoke('zoom:reset'),
+  onDeepLinkImport: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on('deep-link:import', listener);
+    return () => ipcRenderer.removeListener('deep-link:import', listener);
+  },
   onUpdaterStatus: (callback) => {
     const listener = (_event, payload) => callback(payload);
     ipcRenderer.on('updater:status', listener);
