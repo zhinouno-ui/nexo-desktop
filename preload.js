ca05bd94872f23cf9d219352f65938fbb38ec428
zhinouno-ui <zhinouno@gmail.com>
2026-03-09 10:35:52 -0300

    Refactor metrics/export UI and harden store access to remove regressions

--- preload.js
+++ preload.js
@@ -2,29 +2,59 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('nexoStore', {
   getAll: () => ipcRenderer.invoke('store:getAll'),
+  getState: () => ipcRenderer.invoke('store:getAll'),
   setAll: (data) => ipcRenderer.invoke('store:setAll', data),
   patch: (partial) => ipcRenderer.invoke('store:patch', partial),
   backupNow: () => ipcRenderer.invoke('store:backupNow'),
   openDataFolder: () => ipcRenderer.invoke('app:openDataFolder'),
   exportBackup: () => ipcRenderer.invoke('app:exportBackup'),
-  importContactsChunk: (payload) => ipcRenderer.invoke('store:importContactsChunk', payload)
+  importContactsChunk: (payload) => ipcRenderer.invoke('store:importContactsChunk', payload),
+  queueDelta: (delta) => ipcRenderer.invoke('store:queueDelta', delta),
+  flushDeltas: (reason) => ipcRenderer.invoke('store:flushDeltas', reason || 'manual'),
+  asyncSaveRequest: (payload) => ipcRenderer.send('async-save-request', payload || {})
 });
 
 contextBridge.exposeInMainWorld('electronAPI', {
   openExternal: (url) => ipcRenderer.invoke('external:open', url),
+  openImportDialog: () => ipcRenderer.invoke('dialog:openImportFiles'),
+  readTextFile: (filePath) => ipcRenderer.invoke('file:readText', filePath),
+  notify: (payload) => ipcRenderer.invoke('app:notify', payload || {}),
   checkForUpdates: () => ipcRenderer.invoke('updater:check'),
   installUpdate: (options) => ipcRenderer.invoke('updater:install', options || {}),
   rollbackPreviousVersion: () => ipcRenderer.invoke('updater:rollbackPrevious'),
+  getUpdaterDiagnostics: () => ipcRenderer.invoke('updater:diagnostics'),
   getAppVersion: () => ipcRenderer.invoke('app:getVersion'),
+  getRuntimeHash: () => ipcRenderer.invoke('app:getRuntimeHash'),
   getErrorLogPath: () => ipcRenderer.invoke('app:getErrorLogPath'),
   openErrorLog: () => ipcRenderer.invoke('app:openErrorLog'),
   logError: (payload) => ipcRenderer.invoke('app:logError', payload),
+  queueUpload: (payload) => ipcRenderer.invoke('app:queueUpload', payload),
+
+  listProfiles: () => ipcRenderer.invoke('profile:list'),
+  createProfile: (payload) => ipcRenderer.invoke('profile:create', payload || {}),
+  renameProfile: (payload) => ipcRenderer.invoke('profile:rename', payload || {}),
+  deleteProfile: (payload) => ipcRenderer.invoke('profile:delete', payload || {}),
+  resolveImportMode: () => ipcRenderer.invoke('profile:resolveImportMode'),
+  buildExport: (payload) => ipcRenderer.invoke('export:build', payload || {}),
+
   zoomIn: () => ipcRenderer.invoke('zoom:in'),
   zoomOut: () => ipcRenderer.invoke('zoom:out'),
   zoomReset: () => ipcRenderer.invoke('zoom:reset'),
+  onDeepLinkImport: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on('deep-link:import', listener);
+    return () => ipcRenderer.removeListener('deep-link:import', listener);
+  },
   onUpdaterStatus: (callback) => {
     const listener = (_event, payload) => callback(payload);
     ipcRenderer.on('updater:status', listener);
     return () => ipcRenderer.removeListener('updater:status', listener);
   }
+});
+
+contextBridge.exposeInMainWorld('api', {
+  exportDaily: () => ipcRenderer.invoke('export:daily'),
+  exportBackup: () => ipcRenderer.invoke('export:full'),
+  exportFull: () => ipcRenderer.invoke('export:full'),
+  importData: (filePath) => ipcRenderer.invoke('import:data', { filePath })
 });
